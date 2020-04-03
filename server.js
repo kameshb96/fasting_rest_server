fbb144a954033c07df7bc9690dda6c5f71fcda2d
Kamesh <Kamesh>
2020-04-03 02:36:19 -0700

    delete completedfast

--- server.js
+++ server.js
@@ -258,6 +258,58 @@ app.post('/completedFast', (req, res) => {
     })
 })
 
+app.delete('/completedFast', (req,  res) => {
+    let err = {
+        meta: {
+            status: false,
+            message: ""
+        }
+    }
+    let st = req.headers.sessiontoken
+    if (st == undefined || st == "") {
+        err.meta.message = "Please login again"
+        res.status(403).send(err)
+        return
+    }
+
+    if (req.body.id == undefined || req.body.id == "") {
+        err.meta.message = "Please specify an id for the completedFast"
+        res.status(400).send(err)
+        return
+    }
+
+    db.collection('users').findOne({ sessionToken: st }, (error, result) => {
+        if (error) {
+            err.meta.message = error
+            res.status(500).send(err)
+            return
+        }
+        if (!result) {
+            err.meta.message = "Invalid sessionToken"
+            res.status(403).send(err)
+            return
+        }
+        let id = result._id
+        db.collection('completedFasts').deleteOne({ userId: id, _id: ObjectId(req.body.id) }, (e, r) => {
+            if (e) {
+                err.meta.message = e
+                res.status(500).send(err)
+                return
+            }
+            if(r.deletedCount > 0) {
+                err.meta.message = "Success"
+                err.meta.status = true
+                res.status(200).send(err)
+            }
+            else {
+                err.meta.message = "No completedFast found"
+                err.meta.status = false
+                res.status(404).send(err)
+            }
+        })
+    })
+})
+
 app.post('/fast', (req, res) => {
     let err = {
         meta: {
